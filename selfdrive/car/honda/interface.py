baf11ff16550c6b2a91318e74d815648fc537b27
Mostafa Ayesh <mostafaayesh@outlook.com>
2022-06-04 01:09:32 -0400

    use max_lat_accel for torque tuning

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -7,8 +7,9 @@ from selfdrive.car.honda.values import CarControllerParams, CruiseButtons, Honda
 from selfdrive.car import STD_CARGO_KG, CivicParams, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint, get_safety_config
 from selfdrive.car.interfaces import CarInterfaceBase
 from selfdrive.car.disable_ecu import disable_ecu
+from selfdrive.controls.lib.latcontrol_torque import set_torque_tune
 try:
-  from custom_tune import TUNE_TORQUE, MAX_TORQUE, FRICTION, lateralParams_torqueBP, lateralParams_torqueV, lateralTuning_pid_kpV, lateralTuning_pid_kiV
+  from custom_tune import TUNE_TORQUE, MAX_LAT_ACCEL, FRICTION, lateralParams_torqueBP, lateralParams_torqueV, lateralTuning_pid_kpV, lateralTuning_pid_kiV
   CUSTOM_TUNE = True
 except ImportError:
   CUSTOM_TUNE = False
@@ -336,12 +337,7 @@ class CarInterface(CarInterfaceBase):
 
     if CUSTOM_TUNE:
       if TUNE_TORQUE:
-        ret.lateralTuning.init('torque')
-        ret.lateralTuning.torque.useSteeringAngle = True
-        ret.lateralTuning.torque.kp = 2.0 / MAX_TORQUE
-        ret.lateralTuning.torque.kf = 1.0 / MAX_TORQUE
-        ret.lateralTuning.torque.ki = 0.5 / MAX_TORQUE
-        ret.lateralTuning.torque.friction = FRICTION
+        set_torque_tune(ret.lateralTuning, MAX_LAT_ACCEL, FRICTION)
       else:
         ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [lateralParams_torqueBP, lateralParams_torqueV]
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [lateralTuning_pid_kpV, lateralTuning_pid_kiV]
