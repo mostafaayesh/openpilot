12ab39c6ceebb858c9fcff4e4e02c9f0fd5ba732
Mostafa Ayesh <mostafaayesh@outlook.com>
2022-07-07 18:32:51 -0400

    more aggressive acceleration profile
    credits: @arne182

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -21,8 +21,8 @@ from common.params import Params
 LON_MPC_STEP = 0.2  # first step is 0.2s
 AWARENESS_DECEL = -0.2  # car smoothly decel at .2m/s^2 when user is distracted
 A_CRUISE_MIN = -1.2
-A_CRUISE_MAX_VALS = [1.3, 1.2, 1.1, 1.0]
-A_CRUISE_MAX_BP = [0., 15., 25., 40.]
+A_CRUISE_MAX_VALS = [3.5, 3.5, 2.5, 1.5, 2.0, 2.0, 2.0, 1.5, 1.0, 0.5] # accel profile by @arne182 modified by cgw
+A_CRUISE_MAX_BP = [0., 3, 6., 8., 11., 15., 20., 25., 30., 55.] # accel profile by @arne182 modified by cgw
 
 # Lookup table for turns
 _A_TOTAL_MAX_V = [1.7, 3.2]
