c0c62ce4808da9860902d0dae116bfa813178031
Mostafa <mostafaayesh@outlook.com>
2023-04-10 02:18:19 -0400

    increase max accel for pedal

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -27,6 +27,9 @@ class CarInterface(CarInterfaceBase):
     else:
       # NIDECs don't allow acceleration near cruise_speed,
       # so limit limits of pid to prevent windup
+      if CP.enableGasInterceptor:
+        return CarControllerParams.NIDEC_ACCEL_MIN, CarControllerParams.PEDAL_ACCEL_MAX
+      
       ACCEL_MAX_VALS = [CarControllerParams.NIDEC_ACCEL_MAX, 0.2]
       ACCEL_MAX_BP = [cruise_speed - 2., cruise_speed - .2]
       return CarControllerParams.NIDEC_ACCEL_MIN, interp(current_speed, ACCEL_MAX_BP, ACCEL_MAX_VALS)
