756003f40320f99712b70fa93d77c364712ba890
James <91348155+FrogAi@users.noreply.github.com>
2025-07-28 04:00:00 -0700

    Update

--- frogpilot/common/frogpilot_functions.py
+++ frogpilot/common/frogpilot_functions.py
@@ -22,9 +22,10 @@ from openpilot.frogpilot.assets.model_manager import ModelManager
 from openpilot.frogpilot.assets.theme_manager import HOLIDAY_THEME_PATH, ThemeManager
 from openpilot.frogpilot.common.frogpilot_utilities import delete_file, run_cmd, use_konik_server
 from openpilot.frogpilot.common.frogpilot_variables import (
-  ERROR_LOGS_PATH, EXCLUDED_KEYS, KONIK_LOGS_PATH, MODELS_PATH, SCREEN_RECORDINGS_PATH,
+  ERROR_LOGS_PATH, EXCLUDED_KEYS, HD_LOGS_PATH, KONIK_LOGS_PATH, MODELS_PATH, SCREEN_RECORDINGS_PATH,
   THEME_SAVE_PATH, FrogPilotVariables, frogpilot_default_params, get_frogpilot_toggles, params
 )
+from openpilot.frogpilot.system.frogpilot_stats import send_stats
 
 def backup_directory(backup, destination, success_message, fail_message, minimum_backup_size=0, compressed=False):
   in_progress_destination = destination.parent / (destination.name + "_in_progress")
@@ -154,10 +155,13 @@ def frogpilot_boot_functions(build_metadata, params_cache):
     backup_frogpilot(build_metadata)
     backup_toggles(params_cache)
 
+    send_stats()
+
   threading.Thread(target=backup_thread, daemon=True).start()
 
 def setup_frogpilot(build_metadata):
   ERROR_LOGS_PATH.mkdir(parents=True, exist_ok=True)
+  HD_LOGS_PATH.mkdir(parents=True, exist_ok=True)
   KONIK_LOGS_PATH.mkdir(parents=True, exist_ok=True)
   MODELS_PATH.mkdir(parents=True, exist_ok=True)
   SCREEN_RECORDINGS_PATH.mkdir(parents=True, exist_ok=True)
--- frogpilot/frogpilot_process.py
+++ frogpilot/frogpilot_process.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import datetime
 import json
+import os
 import time
 
 import openpilot.system.sentry as sentry
@@ -16,6 +17,7 @@ from openpilot.frogpilot.common.frogpilot_utilities import flash_panda, is_url_p
 from openpilot.frogpilot.common.frogpilot_variables import ERROR_LOGS_PATH, FrogPilotVariables, get_frogpilot_toggles, params, params_cache, params_memory
 from openpilot.frogpilot.controls.frogpilot_planner import FrogPilotPlanner
 from openpilot.frogpilot.controls.lib.frogpilot_tracking import FrogPilotTracking
+from openpilot.frogpilot.system.frogpilot_stats import send_stats
 
 def assets_checks(model_manager, theme_manager):
   if params_memory.get_bool(MODEL_DOWNLOAD_ALL_PARAM):
@@ -64,6 +66,8 @@ def update_checks(manually_updated, model_manager, now, sm, theme_manager, frogp
 def frogpilot_thread():
   config_realtime_process(5, Priority.CTRL_LOW)
 
+  frogpilot_toggles = get_frogpilot_toggles()
+
   error_log = ERROR_LOGS_PATH / "error.txt"
   if error_log.is_file():
     error_log.unlink()
@@ -72,21 +76,20 @@ def frogpilot_thread():
   model_manager = ModelManager()
   theme_manager = ThemeManager()
 
+  toggles_last_updated = datetime.datetime.now()
+
+  pm = messaging.PubMaster(["frogpilotPlan"])
+  sm = messaging.SubMaster(["carControl", "carState", "controlsState", "deviceState", "driverMonitoringState",
+                            "liveLocationKalman", "liveParameters", "managerState", "modelV2",
+                            "pandaStates", "radarState", "frogpilotCarState",
+                            "frogpilotNavigation"],
+                            poll="modelV2", ignore_avg_freq=["radarState"])
+
   assets_checked = False
   run_update_checks = False
   started_previously = False
   time_validated = False
   toggles_updated = False
-
-  frogpilot_toggles = get_frogpilot_toggles()
-
-  toggles_last_updated = datetime.datetime.now()
-
-  pm = messaging.PubMaster(["frogpilotPlan"])
-  sm = messaging.SubMaster(["carControl", "carState", "controlsState", "deviceState", "driverMonitoringState",
-                            "liveLocationKalman", "managerState", "modelV2", "pandaStates", "radarState",
-                            "frogpilotCarState", "frogpilotNavigation"],
-                            poll="modelV2", ignore_avg_freq=["radarState"])
 
   while True:
     sm.update()
@@ -106,6 +109,9 @@ def frogpilot_thread():
 
       if frogpilot_toggles.random_themes:
         theme_manager.update_active_theme(time_validated, frogpilot_toggles, randomize_theme=True)
+
+      if time_validated and is_url_pingable(os.environ.get("STATS_URL", "")):
+        send_stats()
 
       params_memory.put_bool("IsOnroad", False)
 
