1bc3c1d51d76c720123c403711e08a8cca5685d3
Mostafa Ayesh <mostafaayesh@outlook.com>
2025-08-27 12:59:38 -0400

    lateral tuning: adjust PID params

--- opendbc_repo/opendbc/car/honda/interface.py
+++ opendbc_repo/opendbc/car/honda/interface.py
@@ -262,8 +262,9 @@ class CarInterface(CarInterfaceBase):
         # stock filter output values:     0x009F, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108
         # modified filter output values:  0x009F, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108, 0x0400, 0x0480
         # note: max request allowed is 4096, but request is capped at 3840 in firmware, so modifications result in 2x max
-        stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 2560, 8000], [0, 2560, 3840]]
-        stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.3], [0.1]]
+        stock_cp.lateralParams.torqueBP, stock_cp.lateralParams.torqueV = [[0, 2560, 15360], [0, 2560, 3840]]
+        stock_cp.lateralTuning.pid.kpV, stock_cp.lateralTuning.pid.kiV = [[0.1575], [0.05175]]
+        stock_cp.lateralTuning.pid.kf = 0.00004
 
     elif candidate == CAR.HONDA_ACCORD:
       if eps_modified:
