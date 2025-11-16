optoolfactory/c4-frogpilot

Always On Lateral
Co-Authored-By: Jacob Pfeifer <jacob@pfeifer.dev>

opendbc_repo/opendbc/safety/modes/hyundai.h
@@ -175,6 +175,9 @@ static void hyundai_rx_hook(const CANPacket_t *msg) {
     }
 
     // FrogPilot variables
+    if (msg->addr == 0x391) {
+      hyundai_lkas_button_check(GET_BIT(msg, 4U));
+    }
   }
 }
 

mostafaayesh/openpilot

opendbc_repo/opendbc/safety/modes/hyundai.h
@@ -175,6 +175,9 @@ static void hyundai_rx_hook(const CANPacket_t *msg) {
     }
 
     // FrogPilot variables
+    if (msg->addr == 0x391U) {
+      hyundai_lkas_button_check(GET_BIT(msg, 4U));
+    }
   }
 }
 